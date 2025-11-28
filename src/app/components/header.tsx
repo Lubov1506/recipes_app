943b24166385ed5c2cc374e0ae632eb83597c377
Lubov Parfonova <87125430+Lubov1506@users.noreply.github.com>
2025-11-28 15:25:36 +0200

    header

--- src/app/components/header.tsx
+++ src/app/components/header.tsx
@@ -6,14 +6,17 @@ import {
   NavbarItem,
   Button,
 } from "@heroui/react"
+import clsx from "clsx"
 import Image from "next/image"
 import Link from "next/link"
+import { usePathname } from "next/navigation"
 
 export const AcmeLogo = () => {
   return <Image src='/cocktails.png' height={48} width={48} alt='Cocktails' />
 }
 
 export default function Header() {
+  const pathname = usePathname()
   const navItems = [
     { href: "/", title: "Recipes" },
     { href: "/ingredients", title: "Ingredients" },
@@ -27,14 +30,16 @@ export default function Header() {
         <p className='font-bold text-inherit'>ACME</p>
       </NavbarBrand>
       <NavbarContent className='hidden sm:flex gap-4' justify='center'>
-        {navItems.map((item) => (
-          <NavbarItem key={item.href}>
-            <Link color='foreground' href={item.href}>
-              Features
-            </Link>
-          </NavbarItem>
-        ))}
-
+        {navItems.map((item) => {
+          const isActive = pathname === item.href
+          return (
+            <NavbarItem key={item.href}>
+              <Link color='foreground' href={item.href} className={clsx('hover:scale-75 hover:text transition-all duration-200', isActive && 'text-teal-500')}>
+                {item.title}
+              </Link>
+            </NavbarItem>
+          )
+        })}
       </NavbarContent>
       <NavbarContent justify='end'>
         <NavbarItem className='hidden lg:flex'>
